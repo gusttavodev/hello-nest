2798c4274d750343e316b079e8b2a183eb614810
GadsDev <gusttavo212@gmail.com>
2019-06-22 17:46:21 -0300

    Import Schema in Modules

--- api-test/src/categories/categories.module.ts
+++ api-test/src/categories/categories.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategorySchema } from 'dist/categories/category.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Category', schema: CategorySchema}])
+  ],
   controllers: [CategoriesController],
   providers: [CategoriesService]
 })
