1c7b72d7c5d93e6f136ea0e9b706d38a30fcc542
GadsDev <gusttavo212@gmail.com>
2019-06-22 17:33:10 -0300

    Create new Module and Connect Mongo

--- api-test/src/app.module.ts
+++ api-test/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot("mongodb://gadsden:250433@localhost/"+
+    "test-nest?authSource=admin&w=1", { useNewUrlParser: true}),
+    CategoriesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
