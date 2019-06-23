588efd097ac2a16252a6cceccf24a8f43c7867e8
GadsDev <gusttavo212@gmail.com>
2019-06-22 21:19:45 -0300

    Make All MCS

--- api-test/src/app.module.ts
+++ api-test/src/app.module.ts
@@ -3,14 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CategoriesModule } from './categories/categories.module';
+import { PostsModule } from './posts/posts.module';
+import { AuthService } from './auth/auth.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot("mongodb://gadsden:250433@localhost/"+
     "test-nest?authSource=admin&w=1", { useNewUrlParser: true}),
-    CategoriesModule
+    CategoriesModule,
+    PostsModule,
+    UsersModule,
+    AuthModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
