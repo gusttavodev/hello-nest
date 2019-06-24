d7426a4153573b7d87c3564a9f6ecf1d9c59b623
GadsDev <gusttavo212@gmail.com>
2019-06-23 21:30:23 -0300

    Auth strategy

--- api-test/src/auth/auth.module.ts
+++ api-test/src/auth/auth.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import {  PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+
 
 @Module({
+  imports: [ PassportModule.register({
+    defaultStrategy: 'jwt '}),
+    JwtModule.register({
+      secretOrPrivateKey: 'secretKey',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [PassportModule, AuthService],
 })
 export class AuthModule {}
